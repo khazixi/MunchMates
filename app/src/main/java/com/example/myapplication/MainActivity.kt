ea457158d755d4f9f6722e2f2c81ffd5ae9df621
Khazixi <khazixfangirl@gmail.com>
2022-11-12 13:03:36 -0500

    Added a comment

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
+                // Simple Comment Change
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
