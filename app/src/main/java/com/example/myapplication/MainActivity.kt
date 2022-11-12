396609200cf573c28d387f5391cbb951583e74ea
Khazixi <khazixfangirl@gmail.com>
2022-11-12 12:31:17 -0500

    New Android Using Compose

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,21 +1,14 @@
 package com.example.myapplication
 
-// Kotlin Imports
-import  java.util.Timer
-
-// Android Imports
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
@@ -25,12 +18,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
-                // Simple Comment Change
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Vert()
+                    Greeting("Android")
                 }
             }
         }
@@ -38,26 +30,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Vert(modifier: Modifier = Modifier) {
-    Row(
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.Bottom,
-    ) {
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Settings")
-        }
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Schedule")
-        }
-        
-    }
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
 
-
-@Preview(showBackground = false)
+@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        Vert()
+        Greeting("Android")
     }
 }
