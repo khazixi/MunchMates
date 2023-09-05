3e7698f1f56545059858449e6d95c231aef95511
Khazixi <khazixfangirl@gmail.com>
2023-09-04 21:23:58 -0400

    Got some basic backend work done

--- apps/backend/src/db/queries.ts
+++ apps/backend/src/db/queries.ts
@@ -1,2 +1,57 @@
 // NOTE: We can put all of our queries here
 // NOTE: I don't care if you use drizzle or write it raw, just add types so we know what we're using
+
+import { profile } from "console";
+import { eq, and } from "drizzle-orm";
+import { db } from "./db";
+import { invite, users } from './schema'
+
+
+export async function getParticipatingUsers(event_id: number) {
+  return await db
+    .select({
+      name: users.name,
+      gender: users.gender
+    })
+    .from(invite)
+    .where(
+      eq(invite.event, event_id),
+    )
+    .leftJoin(
+      users,
+      and(
+        eq(
+          invite.to,
+          users.user_id
+        ),
+        eq(
+          invite.status,
+          'accepted'
+        )
+      )
+    )
+}
+
+export async function getInvites(user_id: number) {
+  return await db
+    .select()
+    .from(invite)
+    .where(
+      and(
+        eq(invite.to, user_id),
+        eq(invite.status, 'pending')
+      )
+    )
+}
+
+export async function getProfile(user_id: number) {
+  return await db
+    .select()
+    .from(users)
+    .where(
+      eq(
+        users.user_id,
+        user_id
+      )
+    )
+}
--- apps/backend/src/db/schema.ts
+++ apps/backend/src/db/schema.ts
@@ -4,28 +4,29 @@ import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable('users', {
   user_id: integer('user_id').primaryKey(),
-  name: text('name'),
+  name: text('name').notNull(),
   gender: text('geneder', {
     enum: ['male', 'female', 'non-binary', 'other']
-  })
+  }).notNull()
 })
 
 export const event = sqliteTable('event', {
   event_id: integer('event_id').primaryKey(),
   organizer: text('organizer')
+    .notNull()
     .references(() => users.name),
   hall: text('hall', {
     enum: ['Wocester', 'Berkshire', 'Hampsire', 'Franklin']
-  }),
-  timestamp: text('timestamp'), // NOTE: Use time function to get around lack of type
-  duration: text('duration'),
+  }).notNull(),
+  timestamp: text('timestamp').notNull(), // NOTE: Use time function to get around lack of type
+  duration: text('duration').notNull(),
   details: text('details')
 })
 
 export const invite = sqliteTable('invite', {
-  from: integer('from').references(() => users.user_id),
-  to: integer('to').references(() => users.user_id),
-  event: integer('event').references(() => event.event_id),
+  from: integer('from').notNull().references(() => users.user_id),
+  to: integer('to').notNull().references(() => users.user_id),
+  event: integer('event').notNull().references(() => event.event_id),
   status: text('status', {
     enum: ['accepted', 'pending', 'rejected']
   }).notNull()
